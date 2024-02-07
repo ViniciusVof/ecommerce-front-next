af97c597a64b27232b2477a5253b09a926e1db0d
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2024-02-07 11:20:56 -0300

    Aplicação frontend ecommerce nextjs

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/products",
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/app/pagina1/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+function Pagina1() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        teste
+      </Button>
+    </div>
+  );
+}
+
+export default Pagina1;
